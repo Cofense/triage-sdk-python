8bcfbf22c31c74254df7e8a2c2be3caf4b6c2c90
Eddie Lebow <Eddie.Lebow@phishme.com>
2020-09-11 16:31:03 -0400

    Support filter params when fetching resources from Triage

--- cofense_triage/triage_api_client.py
+++ cofense_triage/triage_api_client.py
@@ -10,11 +10,19 @@ class TriageApiClient:
         oauth2_session = OAuth2Session(client_id, client_secret)
         token = oauth2_session.fetch_token("https://tap.phishmecloud.com/oauth/token")
         auth = OAuth2Auth(token, client=oauth2_session)
-        # TODO using OAuth2Auth directly instead of the session forces us to deal with token refreshing manually, which is not implemented yet
+        # TODO using OAuth2Auth directly instead of the session forces us to deal with token refreshing manually, which we have not implemented
 
         self.jsonapi_session = jsonapi_client.Session(
             "https://tap.phishmecloud.com", request_kwargs={"auth": auth}
         )
 
-    def get_document(self, resource_type, resource_id=None):
-        return self.jsonapi_session.get(f"api/public/v2/{resource_type}", resource_id)
+    def get_document(self, resource_type, resource_id=None, filter_params=None):
+        path = f"api/public/v2/{resource_type}"
+
+        if resource_id:
+            return self.jsonapi_session.get(path, resource_id)
+
+        if filter_params:
+            return self.jsonapi_session.iterate(
+                path, jsonapi_client.Filter(**filter_params)
+            )
