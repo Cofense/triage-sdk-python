90f56a5c77a7e836197f16fc35c5bc46b82ad69d
Eddie Lebow <Eddie.Lebow@phishme.com>
2020-09-16 14:27:26 -0400

    Use __getitem__() to fetch document fields
    There was some complication in calculating JSON-ified attribute names
    from names containing underscores, so use __getitem__() instead. This
    doesn't seem to have any complications.
    
    Except for id, which doesn't seem to work with __getitem__().

--- cofense_triage/report.py
+++ cofense_triage/report.py
@@ -12,69 +12,69 @@ class Report:
 
     @property
     def location(self):
-        return self.document.location
+        return self.document["location"]
 
     @property
     def from_address(self):
-        return self.document.from_address
+        return self.document["from_address"]
 
     @property
     def subject(self):
-        return self.document.subject
+        return self.document["subject"]
 
     @property
     def received_at(self):
-        return self.document.received_at
+        return self.document["received_at"]
 
     @property
     def reported_at(self):
-        return self.document.reported_at
+        return self.document["reported_at"]
 
     @property
     def headers(self):
-        return self.document.headers
+        return self.document["headers"]
 
     @property
     def body(self):
-        return self.document.body
+        return self.document["body"]
 
     @property
     def md5(self):
-        return self.document.md5
+        return self.document["md5"]
 
     @property
     def sha256(self):
-        return self.document.sha256
+        return self.document["sha256"]
 
     @property
     def match_priority(self):
-        return self.document.match_priority
+        return self.document["match_priority"]
 
     @property
     def tags(self):
-        return self.document.tags
+        return self.document["tags"]
 
     @property
     def categorization_tags(self):
-        return self.document.categorization_tags
+        return self.document["categorization_tags"]
 
     @property
     def processed_at(self):
-        return self.document.processed_at
+        return self.document["processed_at"]
 
     @property
     def created_at(self):
-        return self.document.created_at
+        return self.document["created_at"]
 
     @property
     def updated_at(self):
-        return self.document.updated_at
+        return self.document["updated_at"]
 
     @property
     def reporter(self):
         from cofense_triage.reporter import Reporter
 
-        return Reporter(self.document.reporter[0])
+        return Reporter(self.document["reporter"][0])
 
     def to_json(self):
         return "TODO"  # TODO
--- cofense_triage/reporter.py
+++ cofense_triage/reporter.py
@@ -4,39 +4,40 @@ class Reporter:
     def __init__(self, document):
         self.document = document
 
+    @property
     def reporter_id(self):
         return self.document.id
 
     @property
     def email(self):
-        return self.document.email
+        return self.document["email"]
 
     @property
     def reports_count(self):
-        return self.document.reports_count
+        return self.document["reports_count"]
 
     @property
     def last_reported_at(self):
-        return self.document.last_reported_at
+        return self.document["last_reported_at"]
 
     @property
     def reputation_score(self):
-        return self.document.reputation_score
+        return self.document["reputation_score"]
 
     @property
     def vip(self):
-        return self.document.vip
+        return self.document["vip"]
 
     @property
     def created_at(self):
-        return self.document.created_at
+        return self.document["created_at"]
 
     @property
     def updated_at(self):
-        return self.document.updated_at
+        return self.document["updated_at"]
 
     @property
     def reports(self):
         from cofense_triage.report import Report
 
-        return (Report(document) for document in self.document.reports)
+        return (Report(document) for document in self.document["reports"])
