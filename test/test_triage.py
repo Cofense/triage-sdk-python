042a7deccde89920a1ba0a3bac614777b4fabfdb
Eddie Lebow <Eddie.Lebow@phishme.com>
2020-12-21 16:16:23 -0500

    Refactor test_triage.py for reusable assertion helper

--- test/test_triage.py
+++ test/test_triage.py
@@ -3,6 +3,15 @@ import re
 import pytest
 
 
+def assert_get(url, expected_path, expected_params):
+    import urllib.parse
+
+    parsed_url = urllib.parse.urlparse(url)
+
+    assert parsed_url.path == expected_path
+    assert urllib.parse.parse_qs(parsed_url.query) == expected_params
+
+
 @pytest.fixture(autouse=True)
 def mock_all_triage_requests(requests_mock, fixture_from_file):
     requests_mock.get(
@@ -14,6 +23,8 @@ def mock_all_triage_requests(requests_mock, fixture_from_file):
 def test_fetch_reporters_by_address(requests_mock, triage):
     list(triage.fetch_reporters_by_address("reporter1@example.com"))
 
-    assert requests_mock.request_history[-1].url.endswith(
-        "/reporters?filter%5Bemail%5D=reporter1@example.com"
+    assert_get(
+        requests_mock.request_history[-1].url,
+        "/api/public/v2/reporters",
+        {"filter[email]": ["reporter1@example.com"]},
     )
