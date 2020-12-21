c9b8de53df99b9ca16df46fa985eeea18ff3acf4
Eddie Lebow <Eddie.Lebow@phishme.com>
2020-12-21 16:02:13 -0500

    Add test_reporter.py

---
+++ test/test_reporter.py
@@ -0,0 +1,11 @@
+def test_parse(requests_mock, triage, fixture_from_file):
+    requests_mock.get(
+        "https://triage.example.com/api/public/v2/reporters",
+        text=fixture_from_file("reporters.json"),
+    )
+
+    reporter = next(triage.fetch_reporters())
+
+    assert reporter.email == "darren@example.com"
+    assert reporter.reputation_score == 15
+    assert reporter.created_at == "2020-09-22T05:52:20.669Z"
