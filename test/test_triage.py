8609b306d0248ac00554d3a2737e7df3502fc040
Eddie Lebow <Eddie.Lebow@phishme.com>
2020-12-21 16:01:30 -0500

    Rewrite test_triage.py to assert API calls only

--- test/test_triage.py
+++ test/test_triage.py
@@ -1,26 +1,19 @@
+import re
+
 import pytest
 
-from cofense_triage import Triage
 
-
-@pytest.fixture
-def triage(mock_oauth_token):
-    return Triage(
-        host="https://triage.example.com",
-        api_version=2,
-        client_id="some-client-id",
-        client_secret="some-client-secret",
-    )
-
-
-def test_fetch_reporters_by_address(requests_mock, triage, fixture_from_file):
+@pytest.fixture(autouse=True)
+def mock_all_triage_requests(requests_mock, fixture_from_file):
     requests_mock.get(
-        "https://triage.example.com/api/public/v2/reporters?filter%5Bemail%5D=reporter1@example.com",
-        text=fixture_from_file("reporters_filter_email.json"),
+        re.compile("https://triage.example.com/"),
+        text=fixture_from_file("empty.json"),
     )
 
-    reporter = next(triage.fetch_reporters_by_address("reporter1@example.com"))
 
-    assert reporter.email == "darren@example.com"
-    assert reporter.reputation_score == 15
-    assert reporter.created_at == "2020-09-22T05:52:20.669Z"
+def test_fetch_reporters_by_address(requests_mock, triage):
+    list(triage.fetch_reporters_by_address("reporter1@example.com"))
+
+    assert requests_mock.request_history[-1].url.endswith(
+        "/reporters?filter%5Bemail%5D=reporter1@example.com"
+    )
