f76802d146fefe30dc41c6a2da1cd15ea1887b18
Eddie Lebow <Eddie.Lebow@phishme.com>
2021-03-01 13:18:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 setuptools.setup(
     name="cofense_triage",
     version="0.1",
-    description="",
-    url="",
-    author="",
-    author_email="",
-    license="",
+    description="Python interface to the Cofense Triage API V2",
+    url="https://github.com/cofense/triage-sdk-python",
+    author="Cofense, Inc.",
+    author_email="oss@cofense.com",
+    license="MIT",
     packages=setuptools.find_packages(),
     package_data={
         "cofense_triage": ["schema.json"]
