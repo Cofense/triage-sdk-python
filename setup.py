f99e423ea0f71c3ffc3456b04dfe18589c46c9ff
Eddie Lebow <Eddie.Lebow@phishme.com>
2021-03-08 15:45:52 -0500

    Update metadata in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
 import setuptools
+import pathlib
+
+
+def read_readme():
+    with open(pathlib.Path(__file__).parent / "README.md") as f:
+        return f.read()
+
 
 setuptools.setup(
     name="cofense_triage",
     version="0.1",
-    description="Python interface to the Cofense Triage API V2",
-    url="https://github.com/cofense/triage-sdk-python",
-    author="Cofense, Inc.",
+    author="Eddie Lebow",
     author_email="oss@cofense.com",
+    maintainer="Cofense, Inc.",
+    maintainer_email="oss@cofense.com",
+    url="https://github.com/cofense/triage-sdk-python",
+    description="Python interface to the Cofense Triage API V2",
+    long_description=read_readme(),
+    long_description_content_type="text/markdown",
     license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
     packages=setuptools.find_packages(),
     package_data={
         "cofense_triage": ["schema.json"]
